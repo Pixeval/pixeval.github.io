0af049a4bdab788982a6b812202331e8949145e3
Summpot <summpot@outlook.com>
2024-09-21 20:05:00 +0800

    Setup starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  site: "https://pixeval.github.io",
+  integrations: [
+    starlight({
+      title: "Pixeval",
+      logo: {
+        src: "./src/assets/logo.png",
+      },
+      social: {
+        github: "https://github.com/Pixeval/Pixeval",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
